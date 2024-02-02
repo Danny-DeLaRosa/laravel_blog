6efb4731a832e15ec69accb1ecf8ebb60466dba0
Danny De La Rosa <danny.devlarosa@gmail.com>
2024-02-01 19:31:46 -0800

    Made routes and links to each route

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias harum error modi cum inventore nesciunt
+            commodi atque explicabo, similique et repellat maiores vero nam quos dolor minus in nemo exercitationem?
+        </p>
+    </article>
+
+    <a href="/">Go back</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias harum error modi cum inventore nesciunt
+            commodi atque explicabo, similique et repellat maiores vero nam quos dolor minus in nemo exercitationem?
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias harum error modi cum inventore nesciunt
+            commodi atque explicabo, similique et repellat maiores vero nam quos dolor minus in nemo exercitationem?
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias harum error modi cum inventore nesciunt
+            commodi atque explicabo, similique et repellat maiores vero nam quos dolor minus in nemo exercitationem?
+        </p>
+    </article>
+</body>
